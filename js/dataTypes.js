affb491f40c8484ad20bd0b5710625a69757970c
mdshakil04 <m.shakil04@yahoo.com>
2025-03-15 21:05:49 +0600

    New Practices start

--- js/dataTypes.js
+++ js/dataTypes.js
@@ -1,1 +1,7 @@
-console.log("From dataTypes")
+// console.log("From dataTypes")
+const myGoal = "Insha-Allah! I will be a programmer"
+// console.log(typeof myGoal)
+const myName = "MD Shakil"
+const myAge = 34;
+const goodMan = true;
+console.log(typeof myName, typeof myAge, typeof goodMan)
