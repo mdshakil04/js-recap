b1d091ef888f6d25ca723442c2ab18da9c46ad08
mdshakil04 <m.shakil04@yahoo.com>
2025-03-16 22:52:44 +0600

    Let vs const

--- js/dataTypes.js
+++ js/dataTypes.js
@@ -6,4 +6,8 @@ const myAge = 34;
 const goodMan = true;
 // console.log(typeof myName, typeof myAge, typeof goodMan)
 const todaysTemparature = 30.45;
-console.log(typeof todaysTemparature)
+// console.log(typeof todaysTemparature)
+let price = 35;
+console.log("Before changed " + price)
+price = 50;
+console.log("After changed " + price)
